2026e2836c09c56e830cd97057c0c99fff58e681
Vinodkumar-Gelot <vinodanajigelot@gmail.com>
2024-06-23 10:44:25 +0530

    Cahnges on dev

--- LearningGit/Controllers/WeatherForecastController.cs
+++ LearningGit/Controllers/WeatherForecastController.cs
@@ -32,5 +32,16 @@ namespace LearningGit.Controllers
             })
             .ToArray();
         }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Getnew()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
